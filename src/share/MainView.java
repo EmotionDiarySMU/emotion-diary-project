7079efe0334e2e43ba4e645d82761e52009a6a28
iee129 <3yjmy89@gmail.com>
2025-11-22 13:28:56 +0900

    View 브랜치 정리: 통계(chart) 관련 코드 제거, 일기 열람 기능만 유지

--- src/share/MainView.java
+++ src/share/MainView.java
@@ -1,50 +1,23 @@
 package share;
 
-import java.awt.CardLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
 import view.ViewWindow;
-import chart.MainApplication;
 
 public class MainView extends JFrame {
 	
-	CardLayout cardLayout;
-	JPanel cardPanel;
-	
 	public MainView() {
-		setTitle("Emotion Diary - View & Statistics");
+		setTitle("Emotion Diary - View");
         setSize(550, 700);
-        
-        JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
-        
-		JButton view = new JButton("열람");
-		JButton chart = new JButton("통계");
-		
-		JButton[] buttons = {view, chart};
-        for (JButton b : buttons) {
-            menuBar.add(b);
-        }
-        
-        cardLayout = new CardLayout();
-        cardPanel = new JPanel(cardLayout);
-        
-        JPanel viewPanel = new ViewWindow();
-        JPanel chartPanel = new MainApplication();
-        
-        cardPanel.add(viewPanel, "view");
-        cardPanel.add(chartPanel, "chart");
-        
-        add(cardPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
-        // 버튼 클릭 시 화면 전환
-        view.addActionListener(e -> cardLayout.show(cardPanel, "view"));
-        chart.addActionListener(e -> cardLayout.show(cardPanel, "chart"));
+        // 일기 열람 패널만 표시
+        JPanel viewPanel = new ViewWindow();
+        add(viewPanel, BorderLayout.CENTER);
 
         setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
