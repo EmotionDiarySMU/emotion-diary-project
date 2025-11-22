bbe37bb3841c7b089cb2d787a014474242b0d060
iee129 <3yjmy89@gmail.com>
2025-11-22 13:24:25 +0900

    View 브랜치 정리: 일기 작성 관련 파일 제거, 열람 및 통계 기능만 유지

--- src/share/MainView.java
+++ src/share/MainView.java
@@ -7,7 +7,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 import view.ViewWindow;
-import write.WriteDiaryGUI;
+import chart.MainApplication;
 
 public class MainView extends JFrame {
 	
@@ -15,17 +15,16 @@ public class MainView extends JFrame {
 	JPanel cardPanel;
 	
 	public MainView() {
-		setTitle("Emotion Diary");
+		setTitle("Emotion Diary - View & Statistics");
         setSize(550, 700);
         
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         
-		JButton write = new JButton("쓰기");
 		JButton view = new JButton("열람");
 		JButton chart = new JButton("통계");
 		
-		JButton[] buttons = {write, view, chart};
+		JButton[] buttons = {view, chart};
         for (JButton b : buttons) {
             menuBar.add(b);
         }
@@ -33,18 +32,15 @@ public class MainView extends JFrame {
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
         
-        JPanel writePanel = new WriteDiaryGUI();
         JPanel viewPanel = new ViewWindow();
         JPanel chartPanel = new MainApplication();
         
-        cardPanel.add(writePanel, "write");
         cardPanel.add(viewPanel, "view");
         cardPanel.add(chartPanel, "chart");
         
         add(cardPanel);
 
         // 버튼 클릭 시 화면 전환
-        write.addActionListener(e -> cardLayout.show(cardPanel, "write"));
         view.addActionListener(e -> cardLayout.show(cardPanel, "view"));
         chart.addActionListener(e -> cardLayout.show(cardPanel, "chart"));
 
