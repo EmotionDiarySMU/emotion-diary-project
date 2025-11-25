c455b9ed7c1fee5d37ad30c5164d83d9a7184173
SilicaSiO2 <isopurple@outlook.com>
2025-11-25 16:28:37 +0900

    main에서 L&F 설정 바꿈 수정하기 화면 감정 이모지 좌측 정렬 안 되는 경우 해결 수정 시 감정 이모지 저장 오류 해결 회원가입, 회원가입 완료 화면 치우침 해결

--- src/share/Main.java
+++ src/share/Main.java
@@ -1,11 +1,21 @@
 package share;
 
+import javax.swing.UIManager;
+
 import DB.DatabaseManager;
 import DB.QuestionDBManager;
 import login.AuthenticationFrame;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		// L&F 설정
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            // L&F 설정 실패 시 오류 출력
+            e.printStackTrace();
+        }
 		
 		boolean success = DatabaseManager.createDatabase();
 		if (success) QuestionDBManager.initializeQuestions();
